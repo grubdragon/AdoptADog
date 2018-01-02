01d697e513536b7c258cadb8e071ed0ff98083ea
ydidwania <didwaniayashvardhan+github@gmail.com>
2018-01-03 02:50:33 +0530

    added upload bare view

--- adopt/views.py
+++ adopt/views.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    render(request,'index.html')
+
+def home(request):
+    render(request,'home.html')
+
+def upload(request):
+
+    if request.method == 'GET':
+        render(request,'upload.html')
+
+    if request.method == 'POST':
+        name = request.POST['name']
+        # Handle other data similarly
+        render(request, 'thankyou.html')
